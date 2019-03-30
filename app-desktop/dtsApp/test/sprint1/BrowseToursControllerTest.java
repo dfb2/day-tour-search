514605d17e824858d398b9fff35e3b9e11221b36
Davíð Freyr Björnsson <david24099@gmail.com>
2019-03-30 22:56:47 +0000

    Drög

---
+++ app-desktop/dtsApp/test/sprint1/BrowseToursControllerTest.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sprint1;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Nafn: Davíð Freyr Björnsson, Háskóli Íslands, netfang: dfb2@hi.is
+ */
+public class BrowseToursControllerTest {
+    
+    public BrowseToursControllerTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+        
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+        
+    }
+    
+    @Before
+    public void setUp() {
+        
+    }
+    
+    @After
+    public void tearDown() {
+        
+    }
+    
+    @Test
+    public void testBrowseToursByKeyword() {
+        
+    }
+    
+    @Test
+    public void testBrowseDistinctTours() {
+        
+    }
+    
+    @Test
+    public void testSearchTwice() {
+        
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
